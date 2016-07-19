237532f00e6d2f722f58f07011f8ecbd07a81c40
Matthew Treinish <mtreinish@kortar.org>
2016-07-19 17:06:17 -0400

    Initial cookiecutter commit

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# == Class: germqtt
+#
+# Full description of class germqtt here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class germqtt {}
